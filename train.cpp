eb10a972e58232d18bd1a2b86ee43db70e71a814
CN PC <timangar015@gmail.com>
2021-11-04 17:21:15 +0100

    training, lr = 0.01, steps = 904

--- train.cpp
+++ train.cpp
@@ -6,9 +6,9 @@
 void train(int epochs, int render_intervall, float epsilon, float decay)
 {
 	chess_environment* env = new chess_environment;
-	agent* loris = new agent(true, 2.5, 0.02);
+	agent* loris = new agent(true, 2.5, 0.01);
 
-	for (unsigned epoch = 1370; epoch < epochs; epoch++)
+	for (unsigned epoch = 0; epoch < epochs; epoch++)
 	{
 		//env->set("3qk3/8/8/8/8/8/8/4K3 w - - 0 1");
 		env->reset();
